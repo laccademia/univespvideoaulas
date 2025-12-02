9547ea1ab7db14b1c6698a6c48657d50a8e3314c
Laccademia <laccademia@gmail.com>
2025-12-02 16:48:42 -0300

    fix: restore standard build configuration and fix syntax errors

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from '../server/_core/index';
-
-// Vercel Serverless Function Handler
-export default async function handler(req, res) {
-    const { app } = await createApp();
-    app(req, res);
-}
